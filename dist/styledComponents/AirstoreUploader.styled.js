9246feaa7870f6be109dfd90a37f2275eaf9ffc8
dmitry <dmitry@scaleflex.cloud>
2018-04-18 18:12:18 +0300

    upload missing files

---
+++ dist/styledComponents/AirstoreUploader.styled.js
@@ -0,0 +1,7 @@
+var _templateObject = _taggedTemplateLiteral(['\n  display: flex;\n  flex-direction: column;\n  height: 100%;\n  fontFamily: Roboto, sans-serif;\n  background: #181830;\n            \n  ::-webkit-scrollbar {\n    height: 6px !important;\n    width: 6px !important;\n  }\n  \n  ::-webkit-scrollbar-thumb {\n    background: rgb(221,221,221);\n    border-radius: 5px;\n  }\n'], ['\n  display: flex;\n  flex-direction: column;\n  height: 100%;\n  fontFamily: Roboto, sans-serif;\n  background: #181830;\n            \n  ::-webkit-scrollbar {\n    height: 6px !important;\n    width: 6px !important;\n  }\n  \n  ::-webkit-scrollbar-thumb {\n    background: rgb(221,221,221);\n    border-radius: 5px;\n  }\n']);
+
+function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+import styled from 'styled-components';
+
+export var Dialog = styled.div(_templateObject);
