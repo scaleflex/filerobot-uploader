f165ee025d1c5f8d60bb389f9e18470809eff299
dmitry <dmitry@scaleflex.cloud>
2018-08-04 16:18:15 +0300

    change to /v1 in config

--- src/AirstoreUploaderPlugin/reducers/uploader.reducer.js
+++ src/AirstoreUploaderPlugin/reducers/uploader.reducer.js
@@ -70,7 +70,7 @@ const _activateTab = (state, activeTab) => ({...state, activeTab });
 const _setUploaderConfig = (state, config = {}) => {
 
   const uploaderConfig = {
-    uploadPath: `https://${config.CONTAINER}.api.airstore.io/upload`,
+    uploadPath: `https://${config.CONTAINER}.api.airstore.io/upload/v1`,
     uploadParams: config.UPLOAD_PARAMS,
     uploadKey: config.AIRSTORE_UPLOAD_KEY,
     openpixKey: config.OPENPIX_KEY,
