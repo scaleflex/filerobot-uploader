8ace915418b50957770990fea308c98a1a9465ee
dmitry <dmitry@scaleflex.cloud>
2019-02-20 17:45:30 +0200

    refactoring webpack configuration

--- config/webpack.js-config.js
+++ config/webpack.js-config.js
@@ -2,11 +2,11 @@ const path = require('path');
 const { ReactLoadablePlugin } = require('react-loadable/webpack');
 
 const reactLoadablePlugin =  new ReactLoadablePlugin({
-  filename: 'build/react-loadable.json'
+  filename: '../build/react-loadable.json'
 });
 
 module.exports = {
-  entry: path.join(__dirname, "projects/js-plugin/index.js"),
+  entry: path.join(__dirname, "../projects/js-plugin/index.js"),
   output: {
     path: path.join(__dirname, "build"),
     filename: "filerobot-uploader-widget.main.[chunkhash].js",
