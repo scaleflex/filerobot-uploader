604fe0f94bff092abf091cff254c97ac2c5cf133
dmitry <dmitry@scaleflex.cloud>
2019-05-17 13:16:38 +0300

    remove babel polyfill and use core js

--- projects/react-plugin/index.js
+++ projects/react-plugin/index.js
@@ -1,4 +1,4 @@
-import "@babel/polyfill";
+import './polyfills';
 import AirstoreUploader from './components/AirstoreUploaderWrapper';
 
 export default AirstoreUploader;
---
+++ projects/react-plugin/polyfills.js
@@ -0,0 +1,2 @@
+import 'core-js/features/array/find';
+import 'core-js/features/array/for-each';
