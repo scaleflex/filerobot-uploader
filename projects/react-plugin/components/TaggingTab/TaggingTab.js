f21f36f78ff561f0458061520ec5730ceaa737e5
dmitry <dmitry@scaleflex.cloud>
2020-01-16 16:54:43 +0200

    fix: nice error message

--- projects/react-plugin/components/TaggingTab/TaggingTab.js
+++ projects/react-plugin/components/TaggingTab/TaggingTab.js
@@ -217,11 +217,13 @@ class TaggingTab extends Component {
           );
         }
       })
-      .catch(e => {
+      .catch(event => {
+        const { response: { data = {} } = {} } = event;
+
         this.setState({
             isLoading: false,
             isGeneratingTags: false,
-            errorMessage: e.msg || e.message || I18n.t('tagging.something_went_wrong_try_again')
+            errorMessage: data.msg || event.message || I18n.t('tagging.something_went_wrong_try_again')
           }
         );
       })
