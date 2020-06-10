a78ff8cedb1957754711d3b7f2b3822260da86d1
dmitry <dmitry@scaleflex.cloud>
2020-06-10 15:54:16 +0200

    test: try to use 2px instead of 1px to force update

--- projects/react-plugin/components/UploadedImagesTab/UploadedImagesTab.js
+++ projects/react-plugin/components/UploadedImagesTab/UploadedImagesTab.js
@@ -257,15 +257,15 @@ class UploadedImagesTab extends Component {
     const modalWrapper = document.querySelector('#modal-wrapper');
     const modalWrapperOffsetLeft = modalWrapper.offsetLeft;
 
-    modalWrapper.style.left = `${modalWrapperOffsetLeft - 1}px`;
-    modalWrapper.style.transform = `translateX(1px)`;
+    modalWrapper.style.left = `${modalWrapperOffsetLeft - 2}px`;
+    modalWrapper.style.transform = `translateX(2px)`;
+    imageGridWrapper.scrollBy(0, 1);
+
     setTimeout(() => {
       modalWrapper.style.left = `${modalWrapperOffsetLeft}px`;
       modalWrapper.style.transform = `translateX(0px)`;
+      imageGridWrapper.scrollBy(0, -1);
     }, 100);
-
-    imageGridWrapper.scrollBy(0, 1);
-    setTimeout(() => imageGridWrapper.scrollBy(0, -1), 100);
   };
 
   onDeleteFile = () => {
